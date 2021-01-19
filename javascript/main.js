c766a23083157b1f82ec19ab0acf2117db2d995b
Max Dickinson <maxsantistadickinson@gmail.com>
2021-01-18 22:26:06 -0300

    Mudando cor do background de acordo com a escolha do usuário.

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+changeToWhite = function () {
+    document.getElementById('home-section').style.backgroundColor = 'white';
+    document.getElementById('studies-section').style.backgroundColor = 'white';
+    document.getElementById('services-section').style.backgroundColor = 'white';
+    document.getElementById('blog-section').style.backgroundColor = 'white';
+
+    document.getElementById('navbar').style.backgroundColor = 'black';
+}
+
+changeToBlue = function () {
+    document.getElementById('home-section').style.backgroundColor = 'blue';
+    document.getElementById('studies-section').style.backgroundColor = 'blue';
+    document.getElementById('services-section').style.backgroundColor = 'blue';
+    document.getElementById('blog-section').style.backgroundColor = 'blue';
+
+    document.getElementById('navbar').style.backgroundColor = 'blue';
+}
+
+changeToGreen = function () {
+    document.getElementById('home-section').style.backgroundColor = 'green';
+    document.getElementById('studies-section').style.backgroundColor = 'green';
+    document.getElementById('services-section').style.backgroundColor = 'green';
+    document.getElementById('blog-section').style.backgroundColor = 'green';
+
+    document.getElementById('navbar').style.backgroundColor = 'green';
+}
